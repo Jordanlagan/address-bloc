06ce9f47391c54101873c666d7699ab39e129f8a
Jordan Lagan <jordanlagan000@gmail.com>
2017-09-21 20:42:42 -0400

    assignment 4: complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -14,9 +14,20 @@ class AddressBook
 			if name < entry.name
 				break
 			end
-			index+= 1
+			index += 1
 		end
 
 		entries.insert(index, Entry.new(name, phone_number, email))
+	end
+
+	def remove_entry(name, phone_number, email)
+
+		index = 0
+		entries.each do |entry|
+			if name == entry.name
+				entries.delete_at(index)
+			end
+			index += 1
+		end
 	end
 end
